malinathani/neutrino_kernel_oneplus_sdm845

drm/msm: Don't copy fence names by default
Based on kerneltoast/android_kernel_google_wahoo@fe23bc0.

Copying fence debug names for frequent users of sync fences causes
measurably significant CPU overhead.

Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
[@0ctobot: Adapted for msm-4.9]
Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

drivers/gpu/drm/msm/sde/sde_fence.c
@@ -198,8 +198,10 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 		return -ENOMEM;
 
 	sde_fence->ctx = fence_ctx;
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	snprintf(sde_fence->name, SDE_FENCE_NAME_SIZE, "sde_fence:%s:%u",
 						sde_fence->ctx->name, val);
+#endif
 	fence_init(&sde_fence->base, &sde_fence_ops, &ctx->lock,
 		ctx->context, val);
 
@@ -243,7 +245,9 @@ int sde_fence_init(struct sde_fence_context *ctx,
 	}
 	memset(ctx, 0, sizeof(*ctx));
 
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
+#endif
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
 	ctx->context = fence_context_alloc(1);

drivers/gpu/msm/kgsl_sync.c
@@ -296,18 +296,22 @@ static void kgsl_sync_timeline_value_str(struct fence *fence,
 	timestamp_queued = kgsl_sync_fence_get_timestamp(ktimeline,
 					KGSL_TIMESTAMP_QUEUED);
 
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	snprintf(str, size, "%u queued:%u retired:%u",
 		ktimeline->last_timestamp,
 		timestamp_queued, timestamp_retired);
+#endif
 
 	kgsl_sync_timeline_put(ktimeline);
 }
 
 static void kgsl_sync_fence_value_str(struct fence *fence, char *str, int size)
 {
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	struct kgsl_sync_fence *kfence = (struct kgsl_sync_fence *)fence;
 
 	snprintf(str, size, "%u", kfence->timestamp);
+#endif
 }
 
 static const char *kgsl_sync_fence_driver_name(struct fence *fence)
@@ -317,32 +321,40 @@ static const char *kgsl_sync_fence_driver_name(struct fence *fence)
 
 static const char *kgsl_sync_timeline_name(struct fence *fence)
 {
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	struct kgsl_sync_fence *kfence = (struct kgsl_sync_fence *)fence;
 	struct kgsl_sync_timeline *ktimeline = kfence->parent;
 
 	return ktimeline->name;
+#else
+	return "";
+#endif
 }
 
 int kgsl_sync_timeline_create(struct kgsl_context *context)
 {
 	struct kgsl_sync_timeline *ktimeline;
 
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	/*
 	 * Generate a name which includes the thread name, thread id, process
 	 * name, process id, and context id. This makes it possible to
 	 * identify the context of a timeline in the sync dump.
 	 */
 	char ktimeline_name[sizeof(ktimeline->name)] = {};
+#endif
 
 	/* Put context when timeline is released */
 	if (!_kgsl_context_get(context))
 		return -ENOENT;
 
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	snprintf(ktimeline_name, sizeof(ktimeline_name),
 		"%s_%d-%.15s(%d)-%.15s(%d)",
 		context->device->name, context->id,
 		current->group_leader->comm, current->group_leader->pid,
 		current->comm, current->pid);
+#endif
 
 	ktimeline = kzalloc(sizeof(*ktimeline), GFP_KERNEL);
 	if (ktimeline == NULL) {
@@ -351,7 +363,9 @@ int kgsl_sync_timeline_create(struct kgsl_context *context)
 	}
 
 	kref_init(&ktimeline->kref);
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	strlcpy(ktimeline->name, ktimeline_name, KGSL_TIMELINE_NAME_LEN);
+#endif
 	ktimeline->fence_context = fence_context_alloc(1);
 	ktimeline->last_timestamp = 0;
 	INIT_LIST_HEAD(&ktimeline->child_list_head);
@@ -580,11 +594,15 @@ long kgsl_ioctl_syncsource_create(struct kgsl_device_private *dev_priv,
 		goto out;
 	}
 
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	snprintf(name, sizeof(name), "kgsl-syncsource-pid-%d",
 			current->group_leader->pid);
+#endif
 
 	kref_init(&syncsource->refcount);
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	strlcpy(syncsource->name, name, KGSL_TIMELINE_NAME_LEN);
+#endif
 	syncsource->private = private;
 	INIT_LIST_HEAD(&syncsource->child_list_head);
 	spin_lock_init(&syncsource->lock);
@@ -861,11 +879,15 @@ void kgsl_syncsource_process_release_syncsources(
 
 static const char *kgsl_syncsource_get_timeline_name(struct fence *fence)
 {
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	struct kgsl_syncsource_fence *sfence =
 			(struct kgsl_syncsource_fence *)fence;
 	struct kgsl_syncsource *syncsource = sfence->parent;
 
 	return syncsource->name;
+#else
+	return "";
+#endif
 }
 
 static bool kgsl_syncsource_enable_signaling(struct fence *fence)

drivers/media/platform/msm/sde/rotator/sde_rotator_sync.c
@@ -52,8 +52,10 @@ struct sde_rot_fence {
 struct sde_rot_timeline {
 	struct kref kref;
 	spinlock_t lock;
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	char name[SDE_ROT_SYNC_NAME_SIZE];
 	char fence_name[SDE_ROT_SYNC_NAME_SIZE];
+#endif
 	u32 next_value;
 	u32 curr_value;
 	u64 context;
@@ -125,9 +127,13 @@ static const char *sde_rot_fence_get_driver_name(struct fence *fence)
 
 static const char *sde_rot_fence_get_timeline_name(struct fence *fence)
 {
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	struct sde_rot_timeline *tl = to_sde_rot_timeline(fence);
 
 	return tl->name;
+#else
+	return "";
+#endif
 }
 
 static bool sde_rot_fence_enable_signaling(struct fence *fence)
@@ -201,8 +207,10 @@ struct sde_rot_timeline *sde_rotator_create_timeline(const char *name)
 		return NULL;
 
 	kref_init(&tl->kref);
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	snprintf(tl->name, sizeof(tl->name), "rot_timeline_%s", name);
 	snprintf(tl->fence_name, sizeof(tl->fence_name), "rot_fence_%s", name);
+#endif
 	spin_lock_init(&tl->lock);
 	tl->context = fence_context_alloc(1);
 	INIT_LIST_HEAD(&tl->fence_list_head);
@@ -257,7 +265,11 @@ void sde_rotator_resync_timeline(struct sde_rot_timeline *tl)
 	spin_lock_irqsave(&tl->lock, flags);
 	val = tl->next_value - tl->curr_value;
 	if (val > 0) {
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 		SDEROT_WARN("flush %s:%d\n", tl->name, val);
+#else
+		SDEROT_WARN("flush: %d\n", val);
+#endif
 		sde_rotator_inc_timeline_locked(tl, val);
 	}
 	spin_unlock_irqrestore(&tl->lock, flags);
@@ -293,7 +305,9 @@ struct sde_rot_sync_fence *sde_rotator_get_sync_fence(
 	list_add_tail(&f->fence_list, &tl->fence_list_head);
 	sde_rotator_get_timeline(tl);
 	spin_unlock_irqrestore(&tl->lock, flags);
+#ifdef CONFIG_SYNC_FENCE_DEBUG
 	snprintf(f->name, sizeof(f->name), "%s_%u", tl->fence_name, val);
+#endif
 
 	if (fence_fd)
 		*fence_fd = sde_rotator_get_sync_fence_fd(

YaAlex3/kernel_xiaomi_sdm439

Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

drivers/gpu/drm/msm/sde/sde_fence.c
@@ -198,8 +198,10 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 		return -ENOMEM;
 
 	sde_fence->ctx = fence_ctx;
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	snprintf(sde_fence->name, SDE_FENCE_NAME_SIZE, "sde_fence:%s:%u",
 						sde_fence->ctx->name, val);
+#endif
 	fence_init(&sde_fence->base, &sde_fence_ops, &ctx->lock,
 		ctx->context, val);
 
@@ -243,7 +245,9 @@ int sde_fence_init(struct sde_fence_context *ctx,
 	}
 	memset(ctx, 0, sizeof(*ctx));
 
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
+#endif
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
 	ctx->context = fence_context_alloc(1);

drivers/gpu/msm/kgsl_sync.c
@@ -296,18 +296,22 @@ static void kgsl_sync_timeline_value_str(struct fence *fence,
 	timestamp_queued = kgsl_sync_fence_get_timestamp(ktimeline,
 					KGSL_TIMESTAMP_QUEUED);
 
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	snprintf(str, size, "%u queued:%u retired:%u",
 		ktimeline->last_timestamp,
 		timestamp_queued, timestamp_retired);
+#endif
 
 	kgsl_sync_timeline_put(ktimeline);
 }
 
 static void kgsl_sync_fence_value_str(struct fence *fence, char *str, int size)
 {
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	struct kgsl_sync_fence *kfence = (struct kgsl_sync_fence *)fence;
 
 	snprintf(str, size, "%u", kfence->timestamp);
+#endif
 }
 
 static const char *kgsl_sync_fence_driver_name(struct fence *fence)
@@ -317,32 +321,40 @@ static const char *kgsl_sync_fence_driver_name(struct fence *fence)
 
 static const char *kgsl_sync_timeline_name(struct fence *fence)
 {
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	struct kgsl_sync_fence *kfence = (struct kgsl_sync_fence *)fence;
 	struct kgsl_sync_timeline *ktimeline = kfence->parent;
 
 	return ktimeline->name;
+#else
+	return "";
+#endif
 }
 
 int kgsl_sync_timeline_create(struct kgsl_context *context)
 {
 	struct kgsl_sync_timeline *ktimeline;
 
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	/*
 	 * Generate a name which includes the thread name, thread id, process
 	 * name, process id, and context id. This makes it possible to
 	 * identify the context of a timeline in the sync dump.
 	 */
 	char ktimeline_name[sizeof(ktimeline->name)] = {};
+#endif
 
 	/* Put context when timeline is released */
 	if (!_kgsl_context_get(context))
 		return -ENOENT;
 
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	snprintf(ktimeline_name, sizeof(ktimeline_name),
 		"%s_%d-%.15s(%d)-%.15s(%d)",
 		context->device->name, context->id,
 		current->group_leader->comm, current->group_leader->pid,
 		current->comm, current->pid);
+#endif
 
 	ktimeline = kzalloc(sizeof(*ktimeline), GFP_KERNEL);
 	if (ktimeline == NULL) {
@@ -351,7 +363,9 @@ int kgsl_sync_timeline_create(struct kgsl_context *context)
 	}
 
 	kref_init(&ktimeline->kref);
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	strlcpy(ktimeline->name, ktimeline_name, KGSL_TIMELINE_NAME_LEN);
+#endif
 	ktimeline->fence_context = fence_context_alloc(1);
 	ktimeline->last_timestamp = 0;
 	INIT_LIST_HEAD(&ktimeline->child_list_head);
@@ -543,7 +557,9 @@ long kgsl_ioctl_syncsource_create(struct kgsl_device_private *dev_priv,
 	int ret = -EINVAL;
 	int id = 0;
 	struct kgsl_process_private *private = dev_priv->process_priv;
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	char name[KGSL_TIMELINE_NAME_LEN];
+#endif
 
 	if (!kgsl_process_private_get(private))
 		return ret;
@@ -554,11 +570,15 @@ long kgsl_ioctl_syncsource_create(struct kgsl_device_private *dev_priv,
 		goto out;
 	}
 
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	snprintf(name, sizeof(name), "kgsl-syncsource-pid-%d",
 			current->group_leader->pid);
+#endif
 
 	kref_init(&syncsource->refcount);
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	strlcpy(syncsource->name, name, KGSL_TIMELINE_NAME_LEN);
+#endif
 	syncsource->private = private;
 	INIT_LIST_HEAD(&syncsource->child_list_head);
 	spin_lock_init(&syncsource->lock);
@@ -835,11 +855,15 @@ void kgsl_syncsource_process_release_syncsources(
 
 static const char *kgsl_syncsource_get_timeline_name(struct fence *fence)
 {
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	struct kgsl_syncsource_fence *sfence =
 			(struct kgsl_syncsource_fence *)fence;
 	struct kgsl_syncsource *syncsource = sfence->parent;
 
 	return syncsource->name;
+#else
+	return "";
+#endif
 }
 
 static bool kgsl_syncsource_enable_signaling(struct fence *fence)

drivers/media/platform/msm/sde/rotator/sde_rotator_sync.c
@@ -52,8 +52,10 @@ struct sde_rot_fence {
 struct sde_rot_timeline {
 	struct kref kref;
 	spinlock_t lock;
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	char name[SDE_ROT_SYNC_NAME_SIZE];
 	char fence_name[SDE_ROT_SYNC_NAME_SIZE];
+#endif
 	u32 next_value;
 	u32 curr_value;
 	u64 context;
@@ -125,9 +127,13 @@ static const char *sde_rot_fence_get_driver_name(struct fence *fence)
 
 static const char *sde_rot_fence_get_timeline_name(struct fence *fence)
 {
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	struct sde_rot_timeline *tl = to_sde_rot_timeline(fence);
 
 	return tl->name;
+#else
+	return "";
+#endif
 }
 
 static bool sde_rot_fence_enable_signaling(struct fence *fence)
@@ -201,8 +207,10 @@ struct sde_rot_timeline *sde_rotator_create_timeline(const char *name)
 		return NULL;
 
 	kref_init(&tl->kref);
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	snprintf(tl->name, sizeof(tl->name), "rot_timeline_%s", name);
 	snprintf(tl->fence_name, sizeof(tl->fence_name), "rot_fence_%s", name);
+#endif
 	spin_lock_init(&tl->lock);
 	tl->context = fence_context_alloc(1);
 	INIT_LIST_HEAD(&tl->fence_list_head);
@@ -257,7 +265,11 @@ void sde_rotator_resync_timeline(struct sde_rot_timeline *tl)
 	spin_lock_irqsave(&tl->lock, flags);
 	val = tl->next_value - tl->curr_value;
 	if (val > 0) {
+#ifdef CONFIG_DRM_SYNC_DEBUG
 		SDEROT_WARN("flush %s:%d\n", tl->name, val);
+#else
+		SDEROT_WARN("flush: %d\n", val);
+#endif
 		sde_rotator_inc_timeline_locked(tl, val);
 	}
 	spin_unlock_irqrestore(&tl->lock, flags);
@@ -293,7 +305,9 @@ struct sde_rot_sync_fence *sde_rotator_get_sync_fence(
 	list_add_tail(&f->fence_list, &tl->fence_list_head);
 	sde_rotator_get_timeline(tl);
 	spin_unlock_irqrestore(&tl->lock, flags);
+#ifdef CONFIG_DRM_SYNC_DEBUG
 	snprintf(f->name, sizeof(f->name), "%s_%u", tl->fence_name, val);
+#endif
 
 	if (fence_fd)
 		*fence_fd = sde_rotator_get_sync_fence_fd(